952ec5f9a391da9030b582fae434b19dd182ff54
unlimmitted <kosnmasss@gmail.com>
2025-06-08 15:15:55 +0300

    update

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,5 +1,37 @@
 <template>
 	<div class="">
+		<div class="table-param">
+			<div
+				v-if="currentUserRole === 'TEACHER'"
+				class="table-param-text"
+				v-text="'Группа'"
+			/>
+			<q-select
+				v-if="currentUserRole === 'TEACHER'"
+				:options="groups"
+				option-label="name"
+				option-value="id"
+				emit-value map-options
+				v-model="selectedGroup"
+				dense style="min-width: 120px"
+			/>
+			<div
+				v-if="currentUserRole === 'TEACHER'"
+				class="table-param-text"
+				v-text="'Предмет'"
+			/>
+			<q-select
+				v-if="currentUserRole === 'TEACHER'"
+				:options="subjects"
+				option-label="name"
+				option-value="id"
+				emit-value
+				map-options
+				v-model="selectedSubject"
+				dense
+				style="min-width: 120px"
+			/>
+		</div>
 		<div class="">
 			<q-table :rows="tableRows">
 				<template v-slot:header="props">
@@ -44,14 +76,50 @@ export default {
 
 	data: () => ({
 		currentUserRole: "",
-		tableRows: []
+		tableRows: [],
+		groups: [],
+		selectedGroup: "",
+		subjects: [],
+		selectedSubject: "",
+		students: [],
+		student: {}
 	}),
 
 	computed: {},
 
-	methods: {},
+	methods: {
+		fetchSubjects() {
+			axios.get("/api/v1/subjects")
+				.then(res => this.subjects = res.data)
+		},
+		fetchStudents() {
+			axios.post("/api/v1/students", {id: this.selectedGroup})
+				.then(response => {
+					this.students = response.data
+				})
+		},
+	},
 
-	watch: {}
+	watch: {
+		selectedGroup() {
+			this.fetchStudents()
+		}
+	},
+
+	mounted() {
+		axios.get("/api/v1/groups")
+			.then(response => {
+				this.groups = response.data
+			})
+		this.fetchSubjects()
+		if (this.currentUserRole !== "TEACHER") {
+			axios.get("/api/v1/me")
+				.then(response => {
+					console.log(response.data)
+					this.student = response.data
+				})
+		}
+	},
 }
 </script>
 
